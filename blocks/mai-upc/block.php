5fb553fa7514e34a2e9d96a6f34cba391b638c11
JiveDig <jivetheuniversal@gmail.com>
2023-01-18 16:46:49 -0500

    register field group in php

--- blocks/mai-upc/block.php
+++ blocks/mai-upc/block.php
@@ -55,136 +55,47 @@ function mai_register_upc_field_group() {
 		return;
 	}
 
-	// acf_add_local_field_group(
-	// 	[
-	// 		'key'    => 'mai_upc_field_group',
-	// 		'title'  => __( 'Mai URL Parameter Content', 'mai-url-parameter-content' ),
-	// 		'fields' => [
-	// 			[
-	// 				'key'     => 'mai_upc_trigger',
-	// 				'label'   => __( 'Trigger', 'mai-url-parameter-content' ),
-	// 				'name'    => 'trigger',
-	// 				'type'    => 'select',
-	// 				'choices' => [
-	// 					'manual' => __( 'Manual (Custom Link)', 'mai-url-parameter-content' ),
-	// 					'load'   => __( 'On Load', 'mai-url-parameter-content' ),
-	// 					'scroll' => __( 'Scroll Distance', 'mai-url-parameter-content' ),
-	// 					'time'   => __( 'Time Delay', 'mai-url-parameter-content' ),
-	// 				],
-	// 			],
-	// 			[
-	// 				'key'     => 'mai_upc_animate',
-	// 				'label'   => __( 'Animation', 'mai-url-parameter-content' ),
-	// 				'name'    => 'animate',
-	// 				'type'    => 'select',
-	// 				'choices' => [
-	// 					'fade' => __( 'Fade In', 'mai-url-parameter-content' ),
-	// 					'up'   => __( 'Slide up', 'mai-url-parameter-content' ),
-	// 					'down' => __( 'Slide down', 'mai-url-parameter-content' ),
-	// 				],
-	// 			],
-	// 			[
-	// 				'key'               => 'mai_upc_distance',
-	// 				'label'             => __( 'Scroll distance', 'mai-url-parameter-content' ),
-	// 				'name'              => 'distance',
-	// 				'type'              => 'number',
-	// 				'default_value'     => 50,
-	// 				'min'               => 0,
-	// 				'max'               => '',
-	// 				'step'              => 1,
-	// 				'append'            => '%',
-	// 				'conditional_logic' => [
-	// 					[
-	// 						[
-	// 							'field'    => 'mai_upc_trigger',
-	// 							'operator' => '==',
-	// 							'value'    => 'scroll',
-	// 						],
-	// 					],
-	// 				],
-	// 			],
-	// 			[
-	// 				'key'               => 'mai_upc_delay',
-	// 				'label'             => __( 'Delay', 'mai-url-parameter-content' ),
-	// 				'name'              => 'delay',
-	// 				'type'              => 'number',
-	// 				'min'               => 0,
-	// 				'max'               => '',
-	// 				'step'              => '.5',
-	// 				'append'            => __( 'seconds', 'mai-url-parameter-content' ),
-	// 				'conditional_logic' => [
-	// 					[
-	// 						[
-	// 							'field'    => 'mai_upc_trigger',
-	// 							'operator' => '==',
-	// 							'value'    => 'time',
-	// 						],
-	// 					],
-	// 				],
-	// 			],
-	// 			[
-	// 				'key'          => 'mai_upc_width',
-	// 				'label'        => __( 'Width', 'mai-url-parameter-content' ),
-	// 				'instructions' => __( 'Accepts any CSS value (px, em, rem, vw, ch, etc.). Using 100% removes margin around content.', 'mai-url-parameter-content' ),
-	// 				'name'         => 'width',
-	// 				'type'         => 'text',
-	// 				'placeholder'  => '600px',
-	// 			],
-	// 			[
-	// 				'key'               => 'mai_upc_repeat',
-	// 				'label'             => __( 'Repeat', 'mai-url-parameter-content' ),
-	// 				'instructions'      => __( 'The time it takes before this popup will be displayed again for the same user. Use 0 to always show, but beware that this may frustrate your website users.', 'mai-url-parameter-content' ),
-	// 				'name'              => 'repeat',
-	// 				'type'              => 'text',
-	// 				'default_value'     => '7 days', // Can't translate. English for `strtotime()`.
-	// 				'conditional_logic' => [
-	// 					[
-	// 						[
-	// 							'field'    => 'mai_upc_trigger',
-	// 							'operator' => '!=',
-	// 							'value'    => 'manual',
-	// 						],
-	// 					],
-	// 				],
-	// 			],
-	// 			[
-	// 				'key'               => 'mai_upc_repeat_roles',
-	// 				'label'             => __( 'Always repeat for user roles', 'mai-url-parameter-content' ),
-	// 				'name'              => 'repeat_roles',
-	// 				'instructions'      => __( 'Select user roles that will always see this popup, regardless of the setting above.', 'mai-url-parameter-content' ),
-	// 				'type'              => 'select',
-	// 				'choices'           => [], // Added later.
-	// 				'return_format'     => 'value',
-	// 				'multiple'          => 1,
-	// 				'ui'                => 1,
-	// 				'ajax'              => 1,
-	// 				'conditional_logic' => [
-	// 					[
-	// 						[
-	// 							'field'    => 'mai_upc_trigger',
-	// 							'operator' => '!=',
-	// 							'value'    => 'manual',
-	// 						],
-	// 					],
-	// 				],
-	// 			],
-	// 			[
-	// 				'key'          => 'mai_upc_link',
-	// 				'label'        => __( 'Link', 'mai-url-parameter-content' ),
-	// 				'instructions' => __( 'Launch this popup by linking any text or button link to the anchor below. The popup must be on the page for the link to work.', 'mai-url-parameter-content' ),
-	// 				'name'         => 'id',
-	// 				'type'         => 'text',
-	// 			],
-	// 		],
-	// 		'location' => [
-	// 			[
-	// 				[
-	// 					'param'    => 'block',
-	// 					'operator' => '==',
-	// 					'value'    => 'acf/mai-popup',
-	// 				],
-	// 			],
-	// 		],
-	// 	]
-	// );
+	acf_add_local_field_group(
+		[
+			'key'    => 'maiupc_field_group',
+			'title'  => __( 'Mai URL Parameter Content', 'mai-url-parameter-content' ),
+			'fields' => [
+				[
+					'key'          => 'maiupc_parameters',
+					'label'        => __( 'URL Parameters', 'mai-url-parameter-content' ),
+					'name'         => 'maiupc',
+					'type'         => 'repeater',
+					'layout'       => 'block',
+					'min'          => 1,
+					'max'          => 0,
+					'button_label' => __( 'Add Parameter', 'mai-url-parameter-content' ),
+					'sub_fields'   => [
+						[
+							'key'             => 'maiupc_key',
+							'label'           => __( 'Parameter', 'mai-url-parameter-content' ),
+							'name'            => 'key',
+							'type'            => 'text',
+							'parent_repeater' => 'maiupc_parameters',
+						],
+						[
+							'key'             => 'maiupc_value',
+							'label'           => __( 'Value', 'mai-url-parameter-content' ),
+							'name'            => 'value',
+							'type'            => 'text',
+							'parent_repeater' => 'maiupc_parameters',
+						],
+					],
+				],
+			],
+			'location' => [
+				[
+					[
+						'param'    => 'block',
+						'operator' => '==',
+						'value'    => 'acf/mai-upc',
+					],
+				],
+			],
+		]
+	);
 }
